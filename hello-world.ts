2efe5a7001eb83d9e9d4dbc1f5078b14861fef16
yatinranga <41532056+yatinranga@users.noreply.github.com>
2020-09-29 22:20:16 +0530

    primitive datatypes of typescript

--- hello-world.ts
+++ hello-world.ts
@@ -1,16 +1,27 @@
 // here a is a variable which stores any kin of datatype
 
 var a;
- 
-a=10;
-a="typescript";
-a=10.50;
-a=true;
+
+a = 10;
+a = "typescript";
+a = 10.50;
+a = true;
 console.log(a);
 
+//different data types
 
 // if you want to add limitations like var b can only store numbers then 
 
-var b : number;
-b=100;
+var b: number;
+b = 100;
 console.log(b);
+
+
+var c: string;
+c = "typescript";
+console.log(c);
+
+var d: Boolean;
+d = true;
+console.log(d);
+
