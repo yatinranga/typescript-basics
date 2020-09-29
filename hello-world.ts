40117cbd370acaa5736e658b25003a91e9ddbc1e
yatinranga <41532056+yatinranga@users.noreply.github.com>
2020-09-29 21:53:11 +0530

    any datatype and particular datatype in typescript

--- hello-world.ts
+++ hello-world.ts
@@ -1,2 +1,16 @@
-var fn = () => "Hello World";
-console.log(fn);
+// here a is a variable which stores any kin of datatype
+
+var a;
+ 
+a=10;
+a="typescript";
+a=10.50;
+a=true;
+console.log(a);
+
+
+// if you want to add limitations like var b can only store numbers then 
+
+var b : number;
+b=100;
+console.log(b);
