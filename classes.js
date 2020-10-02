ab7bd898af380246c9bd2160051dfdef96536074
yatinranga <41532056+yatinranga@users.noreply.github.com>
2020-10-02 14:35:47 +0530

    functions and constructor in TS

--- classes.js
+++ classes.js
@@ -1,11 +1,30 @@
 var Person = /** @class */ (function () {
-    function Person() {
+    // //constructor
+    // constructor() {
+    //     this.firstName = " ";
+    //     this.lastName = " ";
+    // }
+    /**
+     * if you want more than one constructor in TS then you have to remove the other constructors.
+     * In TS unlike any other language you can't overload constructor
+     */
+    //parameterized constructor
+    function Person(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
+    // method
+    Person.prototype.getfullName = function () {
+        return this.firstName + " " + this.lastName;
+    };
     return Person;
 }());
-var aPerson = new Person();
+// this will give error now bcz we don't have default constructor so we have to make a parametrized constructor
+//var aPerson = new Person();
+var aPerson = new Person(" ", " ");
 aPerson.firstName = "yatin";
 aPerson.lastName = "ranga";
 console.log(aPerson.firstName + aPerson.lastName);
+console.log(aPerson.getfullName());
 // if you want to restrict the class variable then
-var bPerson = new Person();
+//var bPerson: Person = new Person();
--- classes.ts
+++ classes.ts
@@ -1,14 +1,42 @@
 class Person {
 
-    firstName;
+    firstName: string;
     lastName;
+
+    // //constructor
+    // constructor() {
+    //     this.firstName = " ";
+    //     this.lastName = " ";
+    // }
+
+    /**
+     * if you want more than one constructor in TS then you have to remove the other constructors.
+     * In TS unlike any other language you can't overload constructor
+     */
+
+
+    //parameterized constructor
+    constructor(firstName: string, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+    // method
+    getfullName() {
+        return this.firstName + " " + this.lastName;
+    }
 }
 
-var aPerson = new Person();
+// this will give error now bcz we don't have default constructor so we have to make a parametrized constructor
+//var aPerson = new Person();
+
+var aPerson = new Person(" ", " ");
 aPerson.firstName = "yatin";
 aPerson.lastName = "ranga";
 console.log(aPerson.firstName + aPerson.lastName);
+console.log(aPerson.getfullName());
 
 
 // if you want to restrict the class variable then
-var bPerson: Person = new Person();
+//var bPerson: Person = new Person();
