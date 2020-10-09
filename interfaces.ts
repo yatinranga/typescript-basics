d951790773ed124577e61faa42ace716ca024e06
yatinranga <41532056+yatinranga@users.noreply.github.com>
2020-10-09 21:31:34 +0530

    interface implementation and duck typing in TS

--- interfaces.ts
+++ interfaces.ts
@@ -4,3 +4,24 @@ interface Person {
 
     getFullName(): string;
 }
+
+class InterfaceImplement implements Person {
+    firstName: string;
+    lastName: string;
+    getFullName(): string {
+        return this.firstName + this.lastName;
+    }
+
+}
+
+let aPerson: Person = new InterfaceImplement();
+
+// this is duck typing
+
+let duckTyping = {
+    firstName: "hello",
+    lastName: "world",
+    getFullName: () => "test"
+};
+
+aPerson = duckTyping;
